15d964d5563703748f4501381b1617de8a074d47
sonjaya-aichat <sonjaya@aichat.com>
2024-05-04 13:27:40 +0700

    update user

--- security/duo2fa/script/deploy.sh
+++ security/duo2fa/script/deploy.sh
@@ -1,11 +1,20 @@
-#/bin/sh 
-echo "copy repo"
-sudo cp -f script/etc/yum.repos.d/duosecurity.repo /etc/yum.repos.d/
-echo "update pub key"
-sudo rpm --import https://duo.com/DUO-GPG-PUBLIC-KEY.asc
-echo "install duo app"
-sudo yum  -y install duo_unix
-echo "copy config"
-sudo cp -rf script/etc/duo/pam_duo.conf /etc/duo/
-sudo cp -rf script/etc/ssh/sshd_config.d/50-redhat.conf /etc/ssh/sshd_config.d/
-sudo cp -rf script/pam.d/* /etc/pam.d/
+#/bin/bash
+if [ `id -u` -ne 0 ]
+  then echo Please run this script as root or using sudo!
+  exit
+  else 
+     echo "copy repo" 
+     cp -f script/etc/yum.repos.d/duosecurity.repo /etc/yum.repos.d/
+
+     echo "update pub key"
+     rpm --import https://duo.com/DUO-GPG-PUBLIC-KEY.asc
+
+     echo "install duo app"
+     yum  -y install duo_unix
+
+     echo "copy config"
+     "cp -rf script/etc/duo/pam_duo.conf /etc/duo/"
+     "cp -rf script/etc/ssh/sshd_config.d/50-redhat.conf /etc/ssh/sshd_config.d/"
+     "cp -rf script/pam.d/* /etc/pam.d/"
+fi
+exit
